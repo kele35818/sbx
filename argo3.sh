7ce2ae8991c56500cabc0477ab3556051859f055
kele35818 <kele35818@gmail.com>
2025-10-18 14:52:56 +0800

    Update argo3.sh

--- argo3.sh
+++ argo3.sh
@@ -14,7 +14,7 @@ export NEZHA_PORT=""            # v1 哪吒不要填写这个。v0 哪吒 agent 
 export NEZHA_KEY="fVrtznHRL9CpkVHuGyGo6n6NEp1tL7Z0"             # v1 哪吒的 NZ_CLIENT_SECRET 或 v0 哪吒 agent 密钥
 
 # --- Argo 隧道配置 ---
-export ARGO_DOMAIN="ar-us.xxxo.qzz.io"           # Argo 域名，留空即启用临时隧道
+export ARGO_DOMAIN="ar.oxxx.qzz.io"           # Argo 域名，留空即启用临时隧道
 export ARGO_AUTH="eyJhIjoiNTA0NmI1ODdjNmU0YmRhN2FlNTM2ZGZjZGVjM2M1NDkiLCJ0IjoiMDRhMWJlNWQtNzkxOC00NDM1LWI3MGYtNzk1YTZkM2FhNmZhIiwicyI6Ik5UZzVPVGN5TmpBdE5qQm1NUzAwTmpGakxXSTJabUV0WmpBeE4yTXhaVFkzTWpreiJ9"             # Argo Token 或 json，留空即启用临时隧道
 
 # --- 其他配置 ---
